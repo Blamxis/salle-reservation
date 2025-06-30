204c49c7ed432d4caf34908e7eeccd9315c18f7b
Blamxis <gavinetm26@gmail.com>
2025-06-30 08:53:40 +0200

    ♻️ refactor: use singleton pattern for Prisma client to avoid multiple instances

--- src/config/prismaClient.js
+++ src/config/prismaClient.js
@@ -1,6 +1,13 @@
 // src/config/prismaClient.js
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (!global.prismaInstance) {
+  prisma = new PrismaClient();
+  global.prismaInstance = prisma;
+} else {
+  prisma = global.prismaInstance;
+}
 
 module.exports = prisma;
